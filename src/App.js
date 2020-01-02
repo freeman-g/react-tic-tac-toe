881e933b13d7304c59f1496725969d6a5cc56365
Blair G <blair@30platforms.com>
2020-01-02 06:54:49 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,74 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Board extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true
+    }
+  }
+
+  renderSquare(i) {
+    return <Square value={this.state.squares[i]} onClick={(() => this.handleClick(i))} />
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice()
+    squares[i] = this.state.xIsNext ? 'X' : 'O'
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    })
+  }
+
+  render() {
+    const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Board />
+        </header>
+      </div>
+    )
+  }
+}
+
 function App() {
   return (
     <div className="App">
@@ -23,4 +91,4 @@ function App() {
   );
 }
 
-export default App;
+export default Game;
