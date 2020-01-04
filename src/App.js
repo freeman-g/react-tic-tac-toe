71e304c09a85796d6ea6164cb2cc82fefd449007
Blair G <blair@30platforms.com>
2020-01-04 09:58:47 -0500

    Add header to main page

--- src/App.js
+++ src/App.js
@@ -63,32 +63,13 @@ class Game extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
+          <h1>React Tic-Tac-Toe</h1>
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <Board />
         </header>
       </div>
     )
   }
-}
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
 }
 
 export default Game;
